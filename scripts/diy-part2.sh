933945244e7f001cb3ee1e6f7a985a1086cca356
yoyo <212647593+frzb2023@users.noreply.github.com>
2025-12-24 14:13:27 +0800

    Uncomment OpenClash installation in diy-part2.sh
    Uncomment OpenClash pre-installation command in script.

--- scripts/diy-part2.sh
+++ scripts/diy-part2.sh
@@ -21,7 +21,7 @@ else
 fi
 
 # 预装 OpenClash（已注释，保持不变）
-# echo "CONFIG_PACKAGE_luci-app-openclash=y" >> .config
+ echo "CONFIG_PACKAGE_luci-app-openclash=y" >> .config
 
 # 删除 package/mtk/drivers/mt_wifi/files/mt7981-default-eeprom/e2p
 rm -f package/mtk/drivers/mt_wifi/files/mt7981-default-eeprom/e2p
